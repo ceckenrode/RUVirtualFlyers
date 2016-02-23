eee24c078ea8ec34f6505ff80d98a4c9992a65a7
Sean Conaty <anxiolydiot@nbp-152-50.nbp.ruw.rutgers.edu>
2016-02-22 19:41:20 -0500

    required packages

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
+var Sequelize = require('sequelize');
+var expressHandlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
+var PORT = process.env.NODE_ENV || 3000;
+var bcrypt = require('bcryptjs');
+var session = require('express-session');
 var express = require('express');
+//requiring passport last
+var passport = require('passport');
+var passportLocal = require('passport-local');
 var app = express();
 var PORT = process.env.NODE_ENV || 3000;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.engine('handlebars',expressHandlebars({
+  defaultLayout :'main'
+}));
+
+app.set('view engine','handlebars');
+
 
 app.get('/', function(req, res) {
   res.send("Server is functioning...");
