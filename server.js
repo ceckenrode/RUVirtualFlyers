b8c540914cb80984352b07dd3949c703a1257e50
Matt <mparman6@yahoo.com>
2016-02-22 20:41:07 -0500

    added views with working routes

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 var express = require('express');
+var expressHandlebars = require('express-handlebars');
 var app = express();
 var PORT = process.env.NODE_ENV || 3000;
 
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res) {
-  res.send("Welcome home!");
+  res.render('home');
 });
 
-app.get('/main', function(req, res) {
-  res.send("This is the main page");
+app.get('/index', function(req, res) {
+  res.render('index');
 });
 
 app.get('/register', function(req, res) {
-  res.send("Please Register");
+  res.render('register');
 });
 
 app.get('/login', function(req, res) {
-  res.send("Login to submit reviews!");
+  res.render('login');
 });
 
 
