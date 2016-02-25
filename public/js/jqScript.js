6b60d041365eb7e359bad692081679325c207540
Sean Conaty <anxiolydiot@Seans-MacBook-Pro.local>
2016-02-25 02:25:00 -0500

    test stuff

--- public/js/jqScript.js
+++ public/js/jqScript.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
 
+
 var rateToInt = parseInt($("#parseThese").val(), 10);
 
 $('#rateSub').click(rateToInt);
+
 
 });
--- server.js
+++ server.js
@@ -9,11 +9,10 @@ var app = express();
 var PORT = process.env.NODE_ENV || 3000;
 var passport = require('passport');
 var passportLocal = require('passport-local');
-require('dotenv').config();
+
 
 app.use("/js", express.static("public/js"));
 app.use("/css", express.static("public/css"));
-app.use("/images", express.static("public/images"));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -23,9 +22,9 @@ app.engine('handlebars', expressHandlebars({
 }));
 app.set('view engine', 'handlebars');
 
-// var connection = new Sequelize ('rutgersLocations','root');
-//new Sequelize(process.env.JAWSDB_URL);
-
+//var connection = new Sequelize ('rutgersLocations','root');
+require('dotenv').config();
+var connection = new Sequelize(process.env.JAWSDB_URL);
 app.use(bodyParser.urlencoded({
   extended :false
 }));
@@ -70,79 +69,94 @@ passport.deserializeUser(function(id, done) {
     done(null, { id: id, name: id });
 });
 
-// var Users = connection.define ('user',{
-//   username : {
-//     type : Sequelize.STRING,
-//     unique : true,
-//     allowNull: false,
-//     updatedAt: 'last_update',
-//     createdAt: 'date_of_creation'
-//   },
-//   password: {
-//     type:Sequelize.STRING,
-//     unique:false,
-//     allowNull:false,
-//    }}, {
-//     hooks: {
-//       beforeCreate : function(input){
-//         input.password = bcrypt.hashSync(input.password,10);
-//       }
-//     }
-// });
-
-// var Places = connection.define ('place',{
-//   place : {
-//     type : Sequelize.STRING,
-//     unique : true,
-//     allowNull: false,
-//     updatedAt: 'last_update',
-//     createdAt: 'date_of_creation'
-//   }
-// });
-
-// var Ratings= connection.define ('rating',{
-//   rating : {
-//     type : Sequelize.INTEGER,
-//     unique : true,
-//     allowNull: true,
-//     updatedAt: 'last_update',
-//     createdAt: 'date_of_creation'
-//   },
-//   userComment: {
-//     type: Sequelize.STRING(65,535),
-//     unique : false,
-//     allowNull: true,
-//     updatedAt: 'last_update',
-//     createdAt: 'date_of_creation'
-//   }
-// });
-
-// Ratings.belongsTo(Places, {foreignKey: 'fk_places'});
-
-// app.get('/',function(req,res){
-//   res.render('login',{msg:req.query.msg});
-// });
-
-// app.post('/save',function(req,res){
-//   Users.create(req.body).then(function(results){
-//     res.redirect('/?msg=Account Created');
-//   }).catch(function(err){
-//     res.redirect('/?msg='+ err.errors[0].message);
-//   });
-// });
-
-// app.post('/check', passport.authenticate('local', {
-//     successRedirect: '/home',
-//     failureRedirect: '/?msg=Login Credentials do not work'
-// }));
-
-app.get('/home', function(req, res){
-  res.render('home');
+var Users = connection.define ('user',{
+  username : {
+    type : Sequelize.STRING,
+    unique : true,
+    allowNull: false,
+    updatedAt: 'last_update',
+    createdAt: 'date_of_creation'
+  },
+  password: {
+    type:Sequelize.STRING,
+    unique:false,
+    allowNull:false,
+    validate: {
+      len: {
+        args: [5,10],
+        msg: "Your password must be between 5-10 characters"
+      },
+      isUppercase: true
+    }
+   }}, {
+    hooks: {
+      beforeCreate : function(input){
+        input.password = bcrypt.hashSync(input.password,10);
+      }
+    }
 });
 
-// app.get('/rate',function(req,res){
-//   res.render('rate',{msg:req.query.msg});
-// });
+var Places = connection.define ('place',{
+  place : {
+    type : Sequelize.STRING,
+    unique : true,
+    allowNull: false,
+    updatedAt: 'last_update',
+    createdAt: 'date_of_creation'
+  }
+});
+
+var Ratings= connection.define ('rating',{
+  rating : {
+    type : Sequelize.INTEGER,
+    unique : true,
+    allowNull: true,
+    updatedAt: 'last_update',
+    createdAt: 'date_of_creation'
+  },
+  userComment: {
+    type: Sequelize.STRING(65,535),
+    unique : false,
+    allowNull: true,
+    updatedAt: 'last_update',
+    createdAt: 'date_of_creation'
+  },
+    category: {
+    type: Sequelize.STRING,
+    unique: false,
+    allowNull: false,
+    updatedAt: 'last_update',
+    createdAt: 'date_of_creation'
+  }
+});
+
+
+Ratings.belongsTo(Places, {foreignKey: 'fk_places'});
+
+app.get('/',function(req,res){
+  res.render('login',{msg:req.query.msg});
+});
+
+app.post('/save',function(req,res){
+  Users.create(req.body).then(function(results){
+    res.redirect('/?msg=Account Created');
+  }).catch(function(err){
+    res.redirect('/?msg='+ err.errors[0].message);
+  });
+});
+
+app.post('/check', passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/?msg=Login Credentials do not work'
+}));
+
+app.get('/home', function(req, res){
+  res.render("home");
+});
+
+app.get('/rate',function(req,res){
+  res.render('rate',{msg:req.query.msg});
+});
 
 // app.post('/saveRating',function(req,res){
 //   Ratings.create(req.body).then(function(results){
@@ -153,7 +167,7 @@ app.get('/home', function(req, res){
 // });
 
 // app.get('/home', function(req, res){
-//   res.send("Returns home");
+//   res.render("home");
 // });
 
 
@@ -163,13 +177,13 @@ app.get('/home', function(req, res){
 //   res.render('home');
 // });
 
-// app.get('/index', function(req, res) {
-//   res.render('index');
-// });
+app.get('/index', function(req, res) {
+  res.render('index');
+});
 
-// app.get('/register', function(req, res) {
-//   res.render('register');
-// });
+app.get('/register', function(req, res) {
+  res.render('register');
+});
 
 
 
@@ -179,8 +193,8 @@ app.get('/home', function(req, res){
 
 
 // force: true is for testing temporary data, could potentially wipe out an existing database once we create the official ones, so it will have to be removed at that point
-// connection.sync({ force: true }).then(function(){
+connection.sync({ force: true }).then(function(){
   app.listen(PORT,function(){
     console.log("Application is listening on PORT %s",PORT);
   });
-// });
+});
