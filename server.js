e2ce0e4e4b34960db2bd2f85206eb568ea183230
Matt <mparman6@yahoo.com>
2016-02-29 21:13:18 -0500

    added links and routes from home to feed

--- server.js
+++ server.js
@@ -199,7 +199,7 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-app.get('/location', function(req, res) {
+app.get('/location/:id', function(req, res) {
   res.render('location');
 });
 
